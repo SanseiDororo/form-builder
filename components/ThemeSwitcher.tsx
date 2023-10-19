d54b6f4622620a6966c86e3f1fa82eaf206a5966
SanseiDororo <philoscopes@gmail.com>
2023-10-19 08:17:11 +0200

    1. Actions for retrieving data from the database added under the /actions folder 2. Card component added to render the data in under the Home page. 3. C 3. B B A C C 2. Card component added under the Home layout for re

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { SunIcon } from '@radix-ui/react-icons'
+import { DesktopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { Tabs, TabsList, TabsTrigger } from '@radix-ui/react-tabs'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
@@ -14,14 +14,22 @@ function ThemeSwitcher() {
     setMounted(true)
   }, [])
 
-  if (!mounted) return null
+  if (!mounted) return null // avoid rehydration errors
 
   return (
     <Tabs defaultValue={theme}>
       <TabsList className="border">
-        <TabsTrigger value="light" onClick={() => setTheme('light')}>
-          <SunIcon className="h-[1.2rem] w-[1.2rem]" />
-        </TabsTrigger>
+        <div className="flex gap-4">
+          <TabsTrigger value="light" onClick={() => setTheme('light')}>
+            <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+          </TabsTrigger>
+          <TabsTrigger value="dark" onClick={() => setTheme('dark')}>
+            <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 transitional-all dark:rotate-0" />
+          </TabsTrigger>
+          <TabsTrigger value="system" onClick={() => setTheme('system')}>
+            <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
+          </TabsTrigger>
+        </div>
       </TabsList>
     </Tabs>
   )
