2ae0949d7b299779605a6caf616afd3130688cca
SanseiDororo <philoscopes@gmail.com>
2023-10-25 08:31:24 +0200

    1. Infinite loop issue with DesignerContextProvider

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -3,27 +3,35 @@ import { Form } from '@prisma/client'
 import PreviewDialogBtn from '@/components/PreviewDialogBtn'
 import PublishFormBtn from '@/components/PublishFormBtn'
 import SaveFormBtn from '@/components/SaveFormBtn'
+import Designer from './Designer'
+import { DndContext, DragOverlay } from '@dnd-kit/core'
+import DragOverlayWrapper from '@/components/DragOverlayWrapper'
 
 const FormBuilder = ({ form }: { form: Form }) => {
   return (
-    <main className="flex flex-col w-full">
-      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-        <h2 className="truncate font-medium">
-          <span className="text-muted-foreground mr-2">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex items-center gap-2">
-          <PreviewDialogBtn />
-          {!form.published && (
-            <>
-              <SaveFormBtn />
-              <PublishFormBtn />
-            </>
-          )}
+    <DndContext>
+      <main className="flex flex-col w-full">
+        <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h2 className="truncate font-medium">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn />
+                <PublishFormBtn />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
         </div>
-      </nav>
-      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]"></div>
-    </main>
+      </main>
+      <DragOverlayWrapper />
+    </DndContext>
   )
 }
 export default FormBuilder
--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -4,7 +4,7 @@ import { MdOutlinePublish } from 'react-icons/md'
 function PublishFormBtn() {
   return (
     <Button className="gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400">
-      <MdOutlinePublish classname="h-4 w-4" />
+      <MdOutlinePublish className="h-4 w-4" />
       Publish
     </Button>
   )
